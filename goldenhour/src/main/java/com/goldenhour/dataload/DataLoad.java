903a9664631120bf23a824e9e8bfc62f2ad11456
Zhi Heng <25006645@siswa.um.edu.my>
2025-11-30 21:45:17 +0800

    Add attendance data loading to DataLoad class

--- goldenhour/src/main/java/com/goldenhour/dataload/DataLoad.java
+++ goldenhour/src/main/java/com/goldenhour/dataload/DataLoad.java
@@ -8,5 +8,6 @@ public class DataLoad {
     public static List<Model> allModels = CSVHandler.readStock();
     public static List<Employee> allEmployees = CSVHandler.readEmployees();
     public static List<Outlet> allOutlets = CSVHandler.readOutlets();
+    public static List<Attendance> allAttendance = CSVHandler.readAttendance();
     //public static List<Sales> allSales = CSVHandler.readSales();
 }
